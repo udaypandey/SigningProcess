b18c315a6733bea47db74c8e278c3761023a4731
Uday <uday.pandey@gmail.com>
2019-06-29 06:18:03 +0100

    Files updated

---
+++ NoTeam2/NoTeam2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoTeam2
+//
+//  Created by Uday Pandey on 29/06/2019.
+//  Copyright © 2019 Uday Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
